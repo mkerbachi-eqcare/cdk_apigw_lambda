e07eeeddbbf135617967066156abb3446c992079
mkerbachi <1@kerbachi.com>
2021-11-12 17:53:36 -0500

    S3 versioned

--- cdk_apigw_lambda_pipeline/cdk_apigw_lambda_pipeline_stack.py
+++ cdk_apigw_lambda_pipeline/cdk_apigw_lambda_pipeline_stack.py
@@ -13,7 +13,6 @@ from aws_cdk import (
 from aws_cdk import core
 
 
-
 class CdkApigwLambdaPipelineeStack(cdk.Stack):
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
