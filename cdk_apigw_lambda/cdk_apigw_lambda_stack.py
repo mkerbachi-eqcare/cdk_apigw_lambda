100dbab7d7a25e14e35863d0961da5e223120759
mkerbachi <1@kerbachi.com>
2021-11-12 18:09:39 -0500

    S3 versioned

--- cdk_apigw_lambda/cdk_apigw_lambda_stack.py
+++ cdk_apigw_lambda/cdk_apigw_lambda_stack.py
@@ -17,7 +17,7 @@ class CdkApigwLambdaStack(cdk.Stack):
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        # API Gateway
         apigw = aws_apigateway.RestApi(
             self,
             "apigw_test"
