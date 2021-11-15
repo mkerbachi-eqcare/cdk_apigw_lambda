adbb0e99452a2fce388f7666c296026affc2eb89
mkerbachi <1@kerbachi.com>
2021-11-15 15:56:36 -0500

    Comment

--- cdk_apigw_lambda_pipeline/cdk_apigw_lambda_pipeline_stack.py
+++ cdk_apigw_lambda_pipeline/cdk_apigw_lambda_pipeline_stack.py
@@ -124,8 +124,8 @@ class CdkApigwLambdaPipelineeStack(cdk.Stack):
                     "lambda:UpdateFunctionCode"
                 ],
                 resources=[
-                    "arn:aws:lambda:us-east-1:059362432186:function:function1"
-                    # "arn:aws:cloudformation:us-east-1:059362432186:stack/CdkApigwLambdaStack/*"
+                    "arn:aws:lambda:us-east-1:059362432186:function:function1",
+                    "arn:aws:lambda:us-east-1:059362432186:function:function1:*"
                 ]
             )
         )
